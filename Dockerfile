ebc2c102154990694c14cfbe52eb1cc61eab822a
Alex Zylman <alex.zylman@clever.com>
2015-03-06 11:08:35 -0800

    don't run as gearman worker

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,6 @@ RUN apt-get update
 RUN apt-get install -y curl build-essential
 RUN go get github.com/tools/godep
 
-# Gearcmd
-RUN curl -L https://github.com/Clever/gearcmd/releases/download/v0.3.1/gearcmd-v0.3.1-linux-amd64.tar.gz | tar xz -C /usr/local/bin --strip-components 1
-
 # copy source code
 RUN mkdir -p /go/src/github.com/Clever/gearman-load-logger
 ADD . /go/src/github.com/Clever/gearman-load-logger
@@ -18,4 +15,4 @@ RUN rm -rf /go/src/github.com/Clever/gearman-load-logger/Godeps/_workspace/pkg/
 RUN godep go install github.com/Clever/gearman-load-logger
 RUN godep go build -o /usr/local/bin/gearman-load-logger github.com/Clever/gearman-load-logger
 
-CMD ["/go/src/github.com/Clever/gearman-load-logger/run_as_worker.sh"]
+CMD ["/usr/local/bin/gearman-load-logger"]
--- main.go
+++ main.go
@@ -2,54 +2,61 @@ package main
 
 import (
 	"flag"
-	"github.com/Clever/gearadmin"
-	"gopkg.in/Clever/kayvee-go.v1"
+	"fmt"
 	"log"
 	"net"
 	"os"
-	"strconv"
+	"time"
+
+	"github.com/Clever/gearadmin"
+	"gopkg.in/Clever/kayvee-go.v1"
 )
+
+func logMetrics(g gearadmin.GearmanAdmin) {
+	statuses, err := g.Status()
+	if err != nil {
+		log.Fatalf("error retrieving gearman status: %s", err)
+	}
+	for _, status := range statuses {
+		fmt.Println(kayvee.FormatLog("gearlogger", "info", "status", map[string]interface{}{
+			"type":     "gauge",
+			"function": status.Function,
+			"running":  status.Running,
+			"total":    status.Total,
+			"workers":  status.AvailableWorkers,
+		}))
+	}
+}
 
 // This script polls gearman and outputs metrics to syslog in
 // a standard format for later parsing
 func main() {
-	hostPtr := flag.String("host", "", "target gearman host")
-	portPtr := flag.Int("port", 4730, "target gearman port")
+	host := flag.String("host", "", "target gearman host")
+	port := flag.Int("port", 4730, "target gearman port")
+	interval := flag.Duration("interval", time.Minute, "interval to log at")
 	flag.Parse()
 
-	if *hostPtr == "" {
+	h := *host
+	if os.Getenv("GEARMAN_HOST") != "" {
+		h = os.Getenv("GEARMAN_HOST")
+	}
+
+	if h == "" {
 		log.Println("must specify host")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	// connect to gearman
-	c, err := net.Dial("tcp", *hostPtr+":"+strconv.Itoa(*portPtr))
+	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", h, *port))
 	if err != nil {
 		panic(err)
 	}
 	defer c.Close()
 	admin := gearadmin.NewGearmanAdmin(c)
 
-	var metrics map[string]interface{}
-
-	// get status and output to syslog
-	status, _ := admin.Status()
-	for _, funcStatus := range status {
-		metrics = make(map[string]interface{})
-
-		metrics["function"] = funcStatus.Function
-		metrics["total_jobs"] = float64(funcStatus.Total)
-		metrics["running_jobs"] = float64(funcStatus.Running)
-		metrics["available_workers"] = float64(funcStatus.AvailableWorkers)
-
-		metrics["worker_load"] = 0.0
-		if funcStatus.AvailableWorkers > 0 {
-			metrics["worker_load"] = metrics["total_jobs"].(float64) / metrics["available_workers"].(float64)
-		} else if funcStatus.Total > 0 {
-			metrics["worker_load"] = 99.0 // some large number to stand in for infinity
-		}
-
-		log.Printf(kayvee.FormatLog("GEARMAN", "INFO", "QUEUE_INFO", metrics))
+	logMetrics(admin)
+	for _ = range time.Tick(*interval) {
+		logMetrics(admin)
 	}
 }
