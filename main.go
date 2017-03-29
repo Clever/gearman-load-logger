063eefb0907304d1f49186422574496cb1fb8e73
Nate Brennand <nate.brennand@clever.com>
2017-03-29 09:38:41 -0700

    small refactor

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/Clever/discovery-go"
 	"github.com/Clever/gearadmin"
-	"gopkg.in/Clever/kayvee-go.v3"
+	"gopkg.in/Clever/kayvee-go.v6"
+)
+
+const (
+	pollInterval = time.Minute
 )
 
 func logMetrics(g gearadmin.GearmanAdmin) {
@@ -33,30 +34,18 @@ func logMetrics(g gearadmin.GearmanAdmin) {
 // This script polls gearman and outputs metrics to syslog in
 // a standard format for later parsing
 func main() {
-	host := flag.String("host", "", "target gearman host")
-	port := flag.Int("port", 4730, "target gearman port")
-	interval := flag.Duration("interval", time.Minute, "interval to log at")
-	flag.Parse()
-
-	h := *host
-	if discoveredHost, err := discovery.Host("gearmand", "tcp"); err == nil {
-		h = discoveredHost
+	gHost, err := discovery.Host("gearmand", "tcp")
+	if err == nil {
+		log.Fatalf("failed to get gearmand host: %s", err)
 	}
 
-	if discoveredPort, err := discovery.Port("gearmand", "tcp"); err == nil {
-		if portInt, err := strconv.Atoi(discoveredPort); err == nil {
-			port = &portInt
-		}
-	}
-
-	if h == "" {
-		log.Println("must specify host")
-		flag.PrintDefaults()
-		os.Exit(1)
+	gPort, err := discovery.Port("gearmand", "tcp")
+	if err != nil {
+		log.Fatalf("failed to get gearmand port: %s", err)
 	}
 
 	// connect to gearman
-	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", h, *port))
+	c, err := net.Dial("tcp", fmt.Sprintf("%s:%s", gHost, gPort))
 	if err != nil {
 		panic(err)
 	}
@@ -64,7 +53,7 @@ func main() {
 	admin := gearadmin.NewGearmanAdmin(c)
 
 	logMetrics(admin)
-	for _ = range time.Tick(*interval) {
+	for range time.Tick(pollInterval) {
 		logMetrics(admin)
 	}
 }
